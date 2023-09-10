e3d27fadee1d51abbc82abb2082d7864ad16b067
Wambugu Kinyua <95144881+wambugu71@users.noreply.github.com>
2023-09-10 15:51:11 +0300

    Add files via upload

---
+++ mainhug.py
@@ -0,0 +1,41 @@
+from metallma2wambugu.huglogin import login
+import sys
+import time
+import streamlit as st
+def chatwithme(prompt):
+    chatbot = login("kenliz1738@gmail.com","Wambugu71?").login()
+    return chatbot.chat(prompt)
+    
+st.title("Meta llama2 70b chat")
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Accept user input
+if prompt := st.chat_input("Ask your question?"):
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+        assistant_response = chatwithme(prompt)
+        # Simulate stream of response with milliseconds delay
+        for chunk in assistant_response.split():
+            full_response += chunk + " "
+            time.sleep(0.05)
+            # Add a blinking cursor to simulate typing
+            message_placeholder.markdown(full_response + "▌")
+        message_placeholder.markdown(full_response)
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
