29ce6b7557d45d831d5da1f9bd54f32de5d66324
Wambugu Kinyua <95144881+wambugu71@users.noreply.github.com>
2023-10-16 23:48:41 +0300

    Update pdf_reader.py

--- pdf_reader.py
+++ pdf_reader.py
@@ -1,9 +1,9 @@
 from PyPDF2 import PdfReader
-
+import streamlit as st
 class read_pdf:
     def __init__(self):
         super(read_pdf, self).__init__()
-        
+     @st.cache_data   
     def read(self, file)-> list:
         reader = PdfReader(file)
         text = []
