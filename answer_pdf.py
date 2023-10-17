59da7fa3d2541fafce893aac6e3aaf7aba724f06
Wambugu Kinyua <95144881+wambugu71@users.noreply.github.com>
2023-10-17 08:04:33 +0300

    Update answer_pdf.py

--- answer_pdf.py
+++ answer_pdf.py
@@ -1,13 +1,13 @@
-from langchain.embeddings import HuggingFaceHubEmbeddings
-from langchain.document_loaders import TextLoader
-from langchain.embeddings.openai import OpenAIEmbeddings
+#from langchain.embeddings import HuggingFaceHubEmbeddings
+#1from langchain.document_loaders import TextLoader
+#from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import Chroma
 from langchain.embeddings import HuggingFaceHubEmbeddings
 from langchain.llms.base import LLM
 from langchain.chains import RetrievalQA
 import os
-import warnings
+#import warnings
 import random
 import string
 HUG_TOKEN= os.environ["HUGGINGFACEHUB_API_TOKEN"] 
@@ -28,6 +28,6 @@ def question_pdf(llm, text, prompt):
     retriever = db.as_retriever()
     qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever)
     #qa("what is data quality")
-    return  qa({"query": f"{prompt}"})
+    return  qa({"query": f"{prompt}"})['result']
     ####
     
